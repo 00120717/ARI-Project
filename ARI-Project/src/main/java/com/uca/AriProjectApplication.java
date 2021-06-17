55bf158bc0c3961aeacc0179ccf8d0772898c748
Marvin Ramirez <00120717@uca.edu.sv>
2021-06-17 11:45:44 -0600

    Creación de estructura base

---
+++ ARI-Project/src/main/java/com/uca/AriProjectApplication.java
@@ -0,0 +1,13 @@
+package com.uca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AriProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AriProjectApplication.class, args);
+	}
+
+}
---
+++ ARI-Project/src/test/java/com/uca/AriProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AriProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
