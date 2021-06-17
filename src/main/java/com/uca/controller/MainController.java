a4d7a33bb3f8a94797333d052d147da3e98d27d0
Oscar Lovato <00127417@uca.edu.sv>
2021-06-17 15:19:39 -0600

    Avance diseño

---
+++ src/main/java/com/uca/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.uca.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/inicio")
+	public ModelAndView initMain() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("home");
+		return mav;
+	}
+}
