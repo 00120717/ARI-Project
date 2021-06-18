9f9b5d9e72505125b058a2f27723cf353d4a2b52
Rober Fuentes <00062217@uca.edu.sv>
2021-06-17 22:58:36 -0600

    read xml

---
+++ src/main/java/com/uca/convert/xml_to_delim.java
@@ -0,0 +1,50 @@
+package com.uca.convert;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.uca.domain.Cliente;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+public class xml_to_delim {
+
+    List<Cliente> list_clientes = new ArrayList<>();
+
+    //read data
+    public void readXML() {
+        try {
+            SAXBuilder builder = new SAXBuilder();
+            File xml = new File("src/main/resources/clientes.xml");
+
+            Document document = builder.build(xml);
+
+            Element root = document.getRootElement();
+            List<Element> list = root.getChildren("cliente");
+
+            //System.out.println("Documento\tPrimer_nombre\tApellido");
+            for (int i = 0; i < list.size(); i++) {
+                Cliente cliente = new Cliente();
+                Element clienteaux = list.get(i);
+
+                cliente.setDocumento(clienteaux.getChildTextTrim("documento"));
+                cliente.setPrimer_nombre(clienteaux.getChildTextTrim("primer_nombre"));
+                cliente.setApellido(clienteaux.getChildTextTrim("apellido"));
+                cliente.setCredit_card(clienteaux.getChildTextTrim("credit_card"));
+                cliente.setTipo(clienteaux.getChildTextTrim("tipo"));
+                cliente.setTelefono(clienteaux.getChildTextTrim("telefono"));
+
+                list_clientes.add(cliente);
+            }
+
+        } catch (JDOMException | IOException ex) {
+            Logger.getLogger(xml_to_delim.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
