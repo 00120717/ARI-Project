a8db569dd0f73ea317f2b0e59bd60ec527249e4a
Oscar Lovato <00127417@uca.edu.sv>
2021-06-18 14:09:20 -0600

    txt a xml

--- src/main/java/com/uca/controller/MainController.java
+++ src/main/java/com/uca/controller/MainController.java
@@ -1,19 +1,35 @@
 package com.uca.controller;
 
+<<<<<<< Updated upstream
 import com.uca.convert.Convert_to_delim;
 import org.json.simple.parser.ParseException;
+=======
+import java.io.Console;
+
+>>>>>>> Stashed changes
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+<<<<<<< Updated upstream
 import java.io.IOException;
+=======
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+import com.uca.convert.TxtToJson;
+import com.uca.convert.TxtToXml;
+>>>>>>> Stashed changes
 
 
 @Controller
 public class MainController {
 
 	@RequestMapping("/")
+<<<<<<< Updated upstream
 	public ModelAndView initMain() throws IOException, ParseException {
 		ModelAndView mav = new ModelAndView();
 		//mav.addObject("jsontext",convert.TextToJson(""));
@@ -22,6 +38,14 @@ public class MainController {
 		Convert_to_delim c = new Convert_to_delim();
 		//c.jsonToTxt("s",';');
 
+=======
+	public ModelAndView initMain() throws Exception {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("home");
+		TxtToXml conv = new TxtToXml();
+		conv.generate("03423423;rober;fuentes;04534534532;GOLD;2343243523\r\n" + 
+				"353452323;alberto;alfaro;0534534523;PLATINUM;3423523432",';');
+>>>>>>> Stashed changes
 		return mav;
 	}
 
--- src/main/java/com/uca/convert/TxtToXml.java
+++ src/main/java/com/uca/convert/TxtToXml.java
@@ -1,5 +1,92 @@
 package com.uca.convert;
 
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Text;
+
+
 public class TxtToXml {
-	
+	public void generate(String text, char delim) throws Exception{
+		
+		String [] cliente = text.split("\\"+delim);
+		System.out.println("TAMAÑO "+cliente.length);
+        if(cliente.length == 0){
+            System.out.println("ERROR empty ArrayList");
+            return;
+            
+        }else{
+
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            DOMImplementation implementation = builder.getDOMImplementation();
+            Document document = implementation.createDocument(null, "clientes", null);
+            document.setXmlVersion("1.0");
+
+            //Main Node
+            Element raiz = document.getDocumentElement();
+            int j = 0; 
+            for(int i=0; i<((cliente.length+1)/6);i+=1){
+            	System.out.println("ITERACION "+cliente[10]);
+                Element clienteNode = document.createElement("cliente"); 
+           
+                Element documentoNode = document.createElement("documento"); 
+                Text nodeDocumentoValue = document.createTextNode(cliente[j]);
+                documentoNode.appendChild(nodeDocumentoValue);      
+           
+                Element nombreNode = document.createElement("primer-nombre"); 
+                Text nodeNombreValue = document.createTextNode(cliente[j+1]);                
+                nombreNode.appendChild(nodeNombreValue);
+                
+                Element apellidoNode = document.createElement("apellido"); 
+                Text nodeApellidoValue = document.createTextNode(cliente[j+2]);                
+                apellidoNode.appendChild(nodeApellidoValue);
+                
+                Element cardNode = document.createElement("credit-card"); 
+                Text nodeCardValue = document.createTextNode(cliente[j+3]);                
+                cardNode.appendChild(nodeCardValue);
+                
+                Element tipoNode = document.createElement("tipo"); 
+                Text nodeTipoValue = document.createTextNode(cliente[j+4]);                
+                tipoNode.appendChild(nodeTipoValue);
+                
+                Element telefonoNode = document.createElement("telefono"); 
+                Text nodeTelefonoValue = document.createTextNode(cliente[j+5]);                
+                telefonoNode.appendChild(nodeTelefonoValue);
+                
+                clienteNode.appendChild(documentoNode);
+                clienteNode.appendChild(nombreNode);
+                clienteNode.appendChild(apellidoNode);
+                clienteNode.appendChild(cardNode);
+                clienteNode.appendChild(tipoNode);
+                clienteNode.appendChild(telefonoNode);
+                
+                //append itemNode to raiz
+                raiz.appendChild(clienteNode); //pegamos el elemento a la raiz "Documento"
+                j+=6;
+            }                
+            //Generate XML
+            Source source = new DOMSource(document);
+            //Indicamos donde lo queremos almacenar
+            Result result = new StreamResult(new java.io.File("clientes.xml")); //nombre del archivo
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.transform(source, result);
+            
+        }
+		
+    }
 }
