7af79b3c9c467052dde4e169bf1cd750781dde11
Marvin Ramirez <00120717@uca.edu.sv>
2021-06-19 20:26:15 -0600

    Texto a Json terminado

--- src/main/java/com/uca/controller/MainController.java
+++ src/main/java/com/uca/controller/MainController.java
@@ -9,6 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import com.uca.convert.TxtToJson;
 import com.uca.convert.TxtToXml;
+import com.uca.convert.textToJson;
 
 import java.io.IOException;
 
@@ -22,6 +23,7 @@ public class MainController {
     TxtToJson txtToJson = new TxtToJson();
     TxtToXml txtToXml = new TxtToXml();
     Convert_to_delim convert_to_delim = new Convert_to_delim();
+    textToJson textToJson = new textToJson();
 
     FileController fileController = new FileController();
 
@@ -30,7 +32,7 @@ public class MainController {
         ModelAndView mav = new ModelAndView();
 
         TxtToXml conv = new TxtToXml();
-        TxtToJson cvt = new TxtToJson();
+ 
 		/*conv.generate("03423423;rober;fuentes;04534534532;GOLD;2343243523\n" + 
 				"353452323;alberto;alfaro;0534534523;PLATINUM;3423523432", ';');*/
         //System.out.println(cvt.TextToJson("03423423;rober;fuentes;04534534532;GOLD;2343243523" , ';'));
@@ -56,7 +58,7 @@ public class MainController {
                         "353452323;alberto;alfaro;0534534523;PLATINUM;3423523432", ';');
             } else
                 //System.out.println("hello txtToJson");
-                txtToJson.TextToJson("03423423;rober;fuentes;04534534532;GOLD;2343243523\n" +
+                textToJson.TextToJson("03423423;rober;fuentes;04534534532;GOLD;2343243523\n" +
                         "353452323;alberto;alfaro;0534534523;PLATINUM;3423523432", ';');
             flagFormatD = "json";
         } else if (file.getContentType().equals("application/json")) {
